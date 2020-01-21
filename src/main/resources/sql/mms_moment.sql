8a825d94e1b344310523239a53b629406c7765b4
tianqin <387956977@qq.com>
2020-01-21 13:54:33 +0800

    before mbg

--- src/main/resources/sql/mms_moment.sql
+++ src/main/resources/sql/mms_moment.sql
@@ -22,12 +22,12 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- mms m:moment
 -- ----------------------------
 DROP TABLE IF EXISTS `mms_moment`;
-CREATE TABLE `ums_user` (
+CREATE TABLE `mms_moment` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `uuid` binary(36) DEFAULT '' COMMENT '朋友圈uuid',
   `ums_user_id` binary(36) DEFAULT '' COMMENT 'ums_user_uuid',
  `content` varchar(200) DEFAULT '' COMMENT '内容限制144个字',
- `album_pics` json(720) DEFAULT '' COMMENT '画册图片，限制为9张，json格式',
+ `album_pics` JSON DEFAULT NULL COMMENT '画册图片，限制为9张，json格式',
  `short_video` varchar(255) DEFAULT '' COMMENT '短视频',
  `long_video` varchar(255) DEFAULT '' COMMENT '长视频',
  `price` decimal(10,2) DEFAULT 0 COMMENT '朋友圈长视频价格(未发布广场)',
@@ -35,8 +35,9 @@ CREATE TABLE `ums_user` (
  `limit_status` int(1) DEFAULT 0 COMMENT '朋友圈权限:0->所有人(需审核)；1->粉丝可见;2->铁粉(好友)可见;3->仅自己可见',
  `delete_status` int(1) DEFAULT 0 COMMENT '删除状态：0->未删除；1->已删除',
  `verify_status` int(1) DEFAULT 0 COMMENT '审核状态：0->未审核；1->审核通过',
+`create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_moment_id` (`moment_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='朋友圈';
+  UNIQUE KEY `idx_uuid` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='朋友圈';
 
 SET FOREIGN_KEY_CHECKS = 1;
